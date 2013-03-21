07341188caefb24b63b0f74d6c2314dd79f8853d
Ruoxing Liu <rxliu@stanford.edu>
2013-03-21 01:05:35 -0700

    updates on test

--- test.php
+++ test.php
@@ -114,8 +114,54 @@
 		
 ?>
 		</div><!--china-->
-		
 		<div id="country3" class="ui-grid-a" style="display: none;">
+			<?php
+			mysql_connect ("mysql-user-master.stanford.edu","ccs147rxliu","DUVRcwnPDTHGhfty") or die ('Cannot connect to MySQL: ' . mysql_error());
+			mysql_select_db ("c_cs147_rxliu") or die ('Cannot connect to the database: ' . mysql_error());
+			
+			
+		
+			$query = "SELECT * FROM list WHERE username = 'India'";
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_assoc($result)) {
+				$rank = $row["Ranking"];
+				if ($rank == 1) {
+				echo "<div class = 'no1' id = 'top'>";
+				echo "<img src = 'numbers/$rank.gif' width = 10%;/>";
+				echo "<a href='".$row["Amazon"]."' target = 'blank' data-role='button' span style='float:right' id = 'buy1' data-theme='a' data-inline = 'true' >Get it on Amazon</a>";
+				echo "<div class = 'video'><iframe width='476' height='268' src='".$row["Youtube"]."' param name='wmode' value='transparent' frameborder='0' allowfullscreen></iframe></div>";
+				echo "<div class = 'title1'>".$row["SongName"]."</div>";
+				echo "<div class = 'singer1'>".$row["Singer"]."</div></div>";
+				}
+				else if($rank% 2 == 0) {
+					echo "<div class = 'ui-block-a' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Amazon"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Amazon</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+				else if($rank% 2 == 1) {
+					echo "<div class = 'ui-block-b' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Amazon"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Amazon</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+		}
+		
+?>
+		</div><!--india-->
+		
+		
+		<div id="country4" class="ui-grid-a" style="display: none;">
 			<?php
 			mysql_connect ("mysql-user-master.stanford.edu","ccs147rxliu","DUVRcwnPDTHGhfty") or die ('Cannot connect to MySQL: ' . mysql_error());
 			mysql_select_db ("c_cs147_rxliu") or die ('Cannot connect to the database: ' . mysql_error());
@@ -160,7 +206,151 @@
 		
 ?>
 		</div><!--korea-->
+		
+	 <!--france-->
+	  <div id="country5" class="ui-grid-a" style="display: none;">
+	 		<?php
+			mysql_connect ("mysql-user-master.stanford.edu","ccs147rxliu","DUVRcwnPDTHGhfty") or die ('Cannot connect to MySQL: ' . mysql_error());
+			mysql_select_db ("c_cs147_rxliu") or die ('Cannot connect to the database: ' . mysql_error());			
+			$query = "SELECT * FROM list WHERE username = 'France'";
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_assoc($result)) {
+				$rank = $row["Ranking"];
+				if ($rank == 1) {
+				echo "<div class = 'no1' id = 'top'>";
+				echo "<img src = 'numbers/$rank.gif' width = 10%;/>";
+				echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' span style='float:right' id = 'buy1' data-theme='a' data-inline = 'true' >Get it on Itunes</a>";
+				echo "<div class = 'video'><iframe width='476' height='268' src='".$row["Youtube"]."' param name='wmode' value='transparent' frameborder='0' allowfullscreen></iframe></div>";
+				echo "<div class = 'title1'>".$row["SongName"]."</div>";
+				echo "<div class = 'singer1'>".$row["Singer"]."</div></div>";
+				}
+				else if($rank% 2 == 0) {
+					echo "<div class = 'ui-block-a' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+				else if($rank% 2 == 1) {
+					echo "<div class = 'ui-block-b' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+		}
+		
+?>
+		</div><!--france-->
+		
+		
+		
+		<!--germany-->
+	  <div id="country6" class="ui-grid-a" style="display: none;">
+	 		<?php
+			mysql_connect ("mysql-user-master.stanford.edu","ccs147rxliu","DUVRcwnPDTHGhfty") or die ('Cannot connect to MySQL: ' . mysql_error());
+			mysql_select_db ("c_cs147_rxliu") or die ('Cannot connect to the database: ' . mysql_error());			
+			$query = "SELECT * FROM list WHERE username = 'Germany'";
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_assoc($result)) {
+				$rank = $row["Ranking"];
+				if ($rank == 1) {
+				echo "<div class = 'no1' id = 'top'>";
+				echo "<img src = 'numbers/$rank.gif' width = 10%;/>";
+				echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' span style='float:right' id = 'buy1' data-theme='a' data-inline = 'true' >Get it on Itunes</a>";
+				echo "<div class = 'video'><iframe width='476' height='268' src='".$row["Youtube"]."' param name='wmode' value='transparent' frameborder='0' allowfullscreen></iframe></div>";
+				echo "<div class = 'title1'>".$row["SongName"]."</div>";
+				echo "<div class = 'singer1'>".$row["Singer"]."</div></div>";
+				}
+				else if($rank% 2 == 0) {
+					echo "<div class = 'ui-block-a' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+				else if($rank% 2 == 1) {
+					echo "<div class = 'ui-block-b' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+		}
+		
+?>
+		</div><!--germany-->
+		
+		
+		<!--Taiwan-->
+	  <div id="country7" class="ui-grid-a" style="display: none;">
+	 		<?php
+			mysql_connect ("mysql-user-master.stanford.edu","ccs147rxliu","DUVRcwnPDTHGhfty") or die ('Cannot connect to MySQL: ' . mysql_error());
+			mysql_select_db ("c_cs147_rxliu") or die ('Cannot connect to the database: ' . mysql_error());			
+			$query = "SELECT * FROM list WHERE username = 'Taiwan'";
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_assoc($result)) {
+				$rank = $row["Ranking"];
+				if ($rank == 1) {
+				echo "<div class = 'no1' id = 'top'>";
+				echo "<img src = 'numbers/$rank.gif' width = 10%;/>";
+				echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' span style='float:right' id = 'buy1' data-theme='a' data-inline = 'true' >Get it on Itunes</a>";
+				echo "<div class = 'video'><iframe width='476' height='268' src='".$row["Youtube"]."' param name='wmode' value='transparent' frameborder='0' allowfullscreen></iframe></div>";
+				echo "<div class = 'title1'>".$row["SongName"]."</div>";
+				echo "<div class = 'singer1'>".$row["Singer"]."</div></div>";
+				}
+				else if($rank% 2 == 0) {
+					echo "<div class = 'ui-block-a' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+				else if($rank% 2 == 1) {
+					echo "<div class = 'ui-block-b' id = 'song_three'>";
+					if ($rank < 10)
+						echo "<img src = 'numbers/$rank.gif' width = 13.6%; />";
+					else echo "<img src = 'numbers/$rank.gif' width = 18%; />";
+					echo "<a href='".$row["Itunes"]."' target = 'blank' data-role='button' id = 'buy' span style='float:right' data-theme='a' data-inline = 'true' data-mini='true'>Get it on Itunes</a>";
+					echo "<div class = 'video_three' ><iframe width='364' height='205' src='".$row["Youtube"]."' frameborder='0' allowfullscreen></iframe></div>";
+					echo "<div class = 'title'>".$row["SongName"]."</div>";
+					echo "<div class = 'singer'>".$row["Singer"]."</div>";
+					echo "</div>";
+				}
+		}
+		
+?>
+		</div><!--taiwan-->
 	</td>
+	
+	
+	
+	
+	
+	<!----------------------------------------------------------flags---------------------------------------------------------------------------------->
+	
+	
+	
 	<td width = "30%">
 		<table border = "0" id = "stripe" >
 		<tr>
@@ -185,7 +375,7 @@
 							$('input[value$="1"]').click()
 						})				
 						
-						</script>
+			</script>
 		<td>
 			<div class="flip-container" id = "china" ontouchstart="this.classList.toggle('hover');">
 				<div class="flipper">
@@ -209,19 +399,27 @@
 						
 			</script>
 			<td>
-			<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+			<div class="flip-container" id = "india" ontouchstart="this.classList.toggle('hover');">
 				<div class="flipper">
-					<div class="front" id = "front">
-						<a href = "India.php"><img src = "flags/India.png"  class = "cover"/></a>
+					<div class="front">
+						<img src = "flags/India.png"  class = "cover"/></a>
 						<p>India</p>
 					</div>
-					<div class="back" id = "back">	
-						<a href = "India.php"><img src = "albums/India.jpg" class = "cover"/> </a>
+					<div class="back">	
+						<img src = "albums/India.jpg" class = "cover"/> </a>
 						<p>India</p>
 					</div>
 				</div>
 			</div>
 		</td><!--india-->
+		<input type="radio" name="countries" value="3" style="visibility:hidden;position:absolute;top:0;left:0" />
+			<script>
+						
+						$("#india").click(function(e) {		
+							$('input[value$="3"]').click()
+						})				
+						
+			</script><!--india-->
 		</tr>
 		<tr>
 		<td>
@@ -238,75 +436,95 @@
 				</div>
 			</div>
 		</td>
-			<input type="radio" name="countries" value="3" style="visibility:hidden;position:absolute;top:0;left:0" />
+			<input type="radio" name="countries" value="4" style="visibility:hidden;position:absolute;top:0;left:0" />
 		<script>
 						$("#korea").click(function(e) {
-							$('input[value$="3"]').click()	
+							$('input[value$="4"]').click()	
 						})				
 						
-		</script>
+		</script><!--korea-->
+		<td>
+			<div class="flip-container" id = "france" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src = "flags/France.png" class = "cover"/></a>
+						<p>France</p>
+					</div>
+					<div class="back">	
+						<img src = "albums/France.jpg" class = "cover"/> </a>
+						<p>France</p>
+					</div>
+				</div>
+			</div>
+		</td>
+			<input type="radio" name="countries" value="5" style="visibility:hidden;position:absolute;top:0;left:0" />
+		<script>
+						$("#france").click(function(e) {
+							$('input[value$="5"]').click()	
+						})				
+						
+		</script><!--france-->
+		<td>
+			<div class="flip-container" id = "germany" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src = "flags/Germany.gif"  class = "cover"/></a>
+						<p>Germany</p>
+					</div>
+					<div class="back">	
+						<img src = "albums/Germany.jpg" class = "cover"/> </a>
+						<p>Germany</p>
+					</div>
+				</div>
+			</div>
+		</td>
+		<input type="radio" name="countries" value="6" style="visibility:hidden;position:absolute;top:0;left:0" />
+		<script>
+						$("#germany").click(function(e) {
+							$('input[value$="6"]').click()	
+						})				
+						
+		</script><!--germany-->
 		</tr>
 		<tr>
-		<td>
-			<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+			<td>
+			<div class="flip-container" id = "taiwan" ontouchstart="this.classList.toggle('hover');">
 				<div class="flipper">
-					<div class="front" id = "front">
-						<a href = "France.php"><img src = "flags/France.png" class = "cover"/></a>
-						<p>France</p>
-					</div>
-					<div class="back" id = "back">	
-						<a href = "France.php"><img src = "albums/France.jpg" class = "cover"/> </a>
-						<p>France</p>
-					</div>
-				</div>
-			</div>
-		</td>
-		
-		<td>
-			<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-				<div class="flipper">
-					<div class="front" id = "front">
-						<a href = "Germany.php"><img src = "flags/Germany.gif"  class = "cover"/></a>
-						<p>Germany</p>
-					</div>
-					<div class="back" id = "back">	
-						<a href = "Germany.php"><img src = "albums/Germany.jpg" class = "cover"/> </a>
-						<p>Germany</p>
-					</div>
-				</div>
-			</div>
-		</td>
-		<td>
-			<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-				<div class="flipper">
-					<div class="front" id = "front">
-						<a href = "Hong Kong.php"><img src = "flags/Hong Kong.png"  class = "cover"/></a>
-						<p>Hong Kong</p>
-					</div>
-					<div class="back" id = "back">	
-						<a href = "Hong Kong.php"><img src = "albums/Hong Kong.jpg" class = "cover"/> </a>
-						<p>Hong Kong</p>
-					</div>
-				</div>
-			</div>
-		</td>
-		<td>
-			<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-				<div class="flipper">
-					<div class="front" id = "front">
-						<a href = "Taiwan.php"><img src = "flags/Taiwan.png"  class = "cover"/></a>
+					<div class="front" >
+						<img src = "flags/Taiwan.png"  class = "cover"/></a>
 						<p>Taiwan</p>
 					</div>
-					<div class="back" id = "back">	
-						<a href = "Taiwan.php"><img src = "albums/Taiwan.jpg" class = "cover"/> </a>
+					<div class="back" >	
+						<img src = "albums/Taiwan.jpg" class = "cover"/> </a>
 						<p>Taiwan</p>
 					</div>
 				</div>
 			</div>
 		</td>
-		
+		<input type="radio" name="countries" value="7" style="visibility:hidden;position:absolute;top:0;left:0" />
+		<script>
+						$("#taiwan").click(function(e) {
+							$('input[value$="7"]').click()	
+						})				
+						
+		</script><!--taiwan-->
+			
+		<td>
+			<div class="flip-container" id = "hk" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src = "flags/Hong Kong.png"  class = "cover"/></a>
+						<p>Hong Kong</p>
+					</div>
+					<div class="back">	
+						<img src = "albums/Hong Kong.jpg" class = "cover"/> </a>
+						<p>Hong Kong</p>
+					</div>
+				</div>
+			</div>
+		</td>
 		</tr>
-			</table>
+	</table><!--videos-->
 		</td>
 		
 		
